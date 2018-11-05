05b2e761c43d0f0c426e4a6143a8bcf90d3b6415
Aidan <pawaidan@activation.net>
2018-11-05 09:13:30 -0500

    Update temp.c

--- temp.c
+++ temp.c
@@ -2,6 +2,76 @@
 #include <wiringPi.h>
 #include <curl/curl.h>
 
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+     
+int main (void) {
+ DIR *dir;
+ struct dirent *dirent;
+ char dev[16];      // Dev ID
+ char devPath[128]; // Path to device
+ char buf[256];     // Data from device
+ char tmpData[6];   // Temp C * 1000 reported by device 
+ char path[] = "/sys/bus/w1/devices"; 
+ char temp[6];
+ ssize_t numRead;
+ 
+ dir = opendir (path);
+ if (dir != NULL)
+ {
+  while ((dirent = readdir (dir)))
+   // 1-wire devices are links beginning with 28-
+   if (dirent->d_type == DT_LNK && 
+     strstr(dirent->d_name, "28-") != NULL) { 
+    strcpy(dev, dirent->d_name);
+    printf("\nDevice: %s\n", dev);
+   }
+        (void) closedir (dir);
+        }
+ else
+ {
+  perror ("Couldn't open the w1 devices directory");
+  return 1;
+ }
+
+        // Assemble path to OneWire device
+ sprintf(devPath, "%s/%s/w1_slave", path, dev);
+ // Read temp continuously
+ // Opening the device's file triggers new reading
+ while(1) {
+  int fd = open(devPath, O_RDONLY);
+  if(fd == -1)
+  {
+   perror ("Couldn't open the w1 device.");
+   return 1;   
+  }
+  strncpy(tmpData, strstr(buf, "t=") + 2, 5); 
+   float tempC = strtof(tmpData, NULL);
+   printf("Device: %s  - ", dev); 
+   printf("Temp: %.3f C  ", tempC / 1000);
+   printf("%.3f F\n\n", (tempC / 1000) * 9 / 5 + 32);
+   temp = tempC;
+  
+  while((numRead = read(fd, buf, 256)) > 0) 
+  {
+   strncpy(tmpData, strstr(buf, "t=") + 2, 5); 
+   float tempC = strtof(tmpData, NULL);
+   printf("Device: %s  - ", dev);
+   if(tempC < temp -1000 || tempC > temp +1000){
+   curlUrl("https://maker.ifttt.com/trigger/alarm_triggered/with/key/cJK2hwgFfB4Wjisu4zbXK8");
+   }
+   printf("Temp: %.3f C  ", tempC / 1000);
+   printf("%.3f F\n\n", (tempC / 1000) * 9 / 5 + 32);
+  }
+  close(fd);
+ } 
+        /* return 0; --never called due to loop */
+}
+
 int curlUrl(char * URL) {
  CURL *curl;
   CURLcode res;
@@ -22,25 +92,4 @@ int curlUrl(char * URL) {
     /* always cleanup */ 
     curl_easy_cleanup(curl);
   }
-}
-int main(int argc, char *argv[])
-{
-  int i;
-  wiringPiSetup () ;
-  pinMode(0, INPUT);
-pinMode (1, OUTPUT);
-pinMode (2, OUTPUT);
-  while(1) {
-    printf("Waiting for reset\n");
-    while(digitalRead(0) == 1);
-  printf("Waiting for event\n");
-    digitalWrite (1, HIGH);
-    curlUrl("https://maker.ifttt.com/trigger/alarm_triggered/with/key/cJK2hwgFfB4Wjisu4zbXK8");
-    printf("Alarm\n");
-    while(digitalRead(0) == 0);
-    digitalWrite (1, LOW);
-    
-  }
-  /*NOTREACHED*/
-  return 0 ;
 }
